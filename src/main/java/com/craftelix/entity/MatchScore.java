fd6a270a5ee8e1a3d5164743b9c22bd908a0d1dc
leofinder <imrhyde@yandex.ru>
2024-09-03 21:43:17 +0500

    refactor: remove annotations

--- src/main/java/com/craftelix/entity/MatchScore.java
+++ src/main/java/com/craftelix/entity/MatchScore.java
@@ -1,14 +1,8 @@
 package com.craftelix.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class MatchScore {
 
     private Match match;
