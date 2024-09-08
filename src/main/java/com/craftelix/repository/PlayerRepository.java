3512a2070b2860d5f7e10781bc9070016cb9a403
leofinder <imrhyde@yandex.ru>
2024-09-08 16:56:56 +0500

    feat: add findByName method

--- src/main/java/com/craftelix/repository/PlayerRepository.java
+++ src/main/java/com/craftelix/repository/PlayerRepository.java
@@ -12,4 +12,9 @@ public class PlayerRepository extends RepositoryBase<Long, Player> {
         super(Player.class, session);
     }
 
+    public Optional<Player> findByName(String name) {
+        Query<Player> query = session.createQuery("from Player where name = :name", Player.class);
+        query.setParameter("name", name);
+        return query.uniqueResultOptional();
+    }
 }
