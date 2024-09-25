39bfb379dd4165216950bc9e497efa628e5ac846
leofinder <imrhyde@yandex.ru>
2024-09-25 08:14:48 +0500

    fix: change constraint

--- src/main/resources/db/scripts/002_ddl_create_matches_table.sql
+++ src/main/resources/db/scripts/002_ddl_create_matches_table.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS matches (
     player_one_id BIGINT REFERENCES players(id),
     player_two_id BIGINT REFERENCES players(id),
     winner_id BIGINT REFERENCES players(id),
-    CONSTRAINT unique_player_combination UNIQUE (player_one_id, player_two_id)
+    CONSTRAINT player_ids_not_equal CHECK (player_one_id <> player_two_id)
 );
